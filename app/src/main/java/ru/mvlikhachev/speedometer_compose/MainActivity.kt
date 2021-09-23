72d78da4347cc4d594a00df9e88082cec5c08967
MV.Likhachev <m.lihachev@paysend.com>
2021-09-23 23:05:00 +0300

    finish implemented compose speedometer

--- app/src/main/java/ru/mvlikhachev/speedometer_compose/MainActivity.kt
+++ app/src/main/java/ru/mvlikhachev/speedometer_compose/MainActivity.kt
@@ -3,19 +3,48 @@ package ru.mvlikhachev.speedometer_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.TextField
 import ru.mvlikhachev.speedometer_compose.ui.theme.SpeedometercomposeTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            SpeedometercomposeTheme {
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally
+            )  {
+                SpeedometercomposeTheme {
+                    var value by remember { mutableStateOf(0) }
 
+                    CustomComponent(
+                        indicatorValue = value
+                    )
+
+                    TextField(
+                        value = value.toString(),
+                        onValueChange = {
+                            value = if (it.isNotEmpty()) {
+                                it.toInt()
+                            } else {
+                                0
+                            }
+                        },
+                        keyboardOptions = KeyboardOptions(
+                            keyboardType = KeyboardType.Number
+                        )
+                    )
+                }
             }
         }
     }
